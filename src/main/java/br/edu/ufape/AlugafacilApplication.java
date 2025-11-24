905b63414e7f482163dd9fd15bd9b1b2993ac3bf
Carla-Daniela <daninevesdn22@gmail.com>
2025-11-23 21:35:01 -0300

    subindo a estrutura inicial do back seguindo as orientações da disciplina web

---
+++ src/main/java/br/edu/ufape/AlugafacilApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.alugafacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlugafacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlugafacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ufape/alugafacil/AlugafacilApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.alugafacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlugafacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
