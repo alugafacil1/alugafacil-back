71016331ec60297cc611b2498a4d18ed6e568be7
LuoLuann <luo.luann@gmail.com>
2025-12-15 15:48:15 -0300

    config: adicionado notaçaõ para ler o timestamps

--- src/main/java/br/edu/ufape/alugafacil/AlugafacilApplication.java
+++ src/main/java/br/edu/ufape/alugafacil/AlugafacilApplication.java
@@ -2,8 +2,10 @@ package br.edu.ufape.alugafacil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AlugafacilApplication {
 
 	public static void main(String[] args) {
